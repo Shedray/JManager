702c05ff9e4533496d03bd03829877fe9883e4b1
Shedray <1348296317@qq.com>
2018-11-02 19:48:41 +0800

    更改ui样式

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/Main.fxml"));
         primaryStage.setTitle("JManager");
-        primaryStage.setScene(new Scene(root, 800, 600));
-        primaryStage.show();
 
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setMaximized(true);
+        primaryStage.show();
     }
 
 
